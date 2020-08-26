b38b4c38ae8f717ea1a34ad505be4bc63c2a24ab
empering <empering@naver.com>
2020-08-26 17:21:32 +0900

    ModelMapper 추가

--- src/main/java/kr/re/kari/portal/board/BoardApplication.java
+++ src/main/java/kr/re/kari/portal/board/BoardApplication.java
@@ -1,9 +1,11 @@
 package kr.re.kari.portal.board;
 
 import kr.re.kari.portal.board.mapper.BaseMapper;
+import org.modelmapper.ModelMapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan(basePackages = "kr.re.kari.portal.board", basePackageClasses = BaseMapper.class)
@@ -13,4 +15,8 @@ public class BoardApplication {
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
