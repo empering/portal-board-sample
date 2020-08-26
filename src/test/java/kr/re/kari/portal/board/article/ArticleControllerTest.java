97d3db2d662cefa96f8580028df0c9387f58176c
empering <empering@naver.com>
2020-08-27 00:28:45 +0900

    REST docs 생성

--- src/test/java/kr/re/kari/portal/board/article/ArticleControllerTest.java
+++ src/test/java/kr/re/kari/portal/board/article/ArticleControllerTest.java
@@ -1,7 +1,6 @@
 package kr.re.kari.portal.board.article;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import kr.re.kari.portal.board.article.ArticleDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,13 +9,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -51,10 +54,32 @@ class ArticleControllerTest {
 	@Test
 	public void getArticleAll() throws Exception {
 		mockMvc.perform(
-				get("/board/{boardId}/article", TEST_BOARD_ID))
+				get("/board/{boardId}/article", TEST_BOARD_ID)
+						.accept(MediaType.APPLICATION_JSON)
+		)
 				.andExpect(status().isOk())
 				.andDo(print())
-				.andDo(document("getArticleAll"))
+				.andDo(document("get-article-all",
+						pathParameters(
+								parameterWithName("boardId").description("게시판ID")
+						),
+						responseFields(
+								fieldWithPath("[]").description("게시물 목록")
+						).andWithPrefix("[].",
+								fieldWithPath("articleId").type(JsonFieldType.NUMBER).description("게시물ID"),
+								fieldWithPath("parentArticleId").type(JsonFieldType.NUMBER).description("부모게시물ID"),
+								fieldWithPath("boardId").type(JsonFieldType.NUMBER).description("게시판ID"),
+								fieldWithPath("noticeYn").type(JsonFieldType.STRING).description("공지여부"),
+								fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
+								fieldWithPath("contents").type(JsonFieldType.STRING).description("내용"),
+								fieldWithPath("readCount").type(JsonFieldType.NUMBER).description("조회수"),
+								fieldWithPath("useYn").type(JsonFieldType.STRING).description("사용여부"),
+								fieldWithPath("registerId").type(JsonFieldType.STRING).description("등록자"),
+								fieldWithPath("registerTimestamp").type(JsonFieldType.STRING).description("등록일시"),
+								fieldWithPath("updateId").type(JsonFieldType.VARIES).description("수정자"),
+								fieldWithPath("updateTimestamp").type(JsonFieldType.VARIES).description("수정일시")
+						)
+				))
 		;
 	}
 
@@ -62,10 +87,31 @@ class ArticleControllerTest {
 	public void getArticle() throws Exception {
 		mockMvc.perform(
 				get("/board/{boardId}/article/{articleId}",
-						TEST_BOARD_ID, TEST_ARTICLE_ID))
+						TEST_BOARD_ID, TEST_ARTICLE_ID)
+						.accept(MediaType.APPLICATION_JSON)
+		)
 				.andExpect(status().isOk())
 				.andDo(print())
-				.andDo(document("getArticle"))
+				.andDo(document("get-article",
+						pathParameters(
+								parameterWithName("boardId").description("게시판ID"),
+								parameterWithName("articleId").description("게시물ID")
+						),
+						responseFields(
+								fieldWithPath("articleId").type(JsonFieldType.NUMBER).description("게시물ID"),
+								fieldWithPath("parentArticleId").type(JsonFieldType.NUMBER).description("부모게시물ID"),
+								fieldWithPath("boardId").type(JsonFieldType.NUMBER).description("게시판ID"),
+								fieldWithPath("noticeYn").type(JsonFieldType.STRING).description("공지여부"),
+								fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
+								fieldWithPath("contents").type(JsonFieldType.STRING).description("내용"),
+								fieldWithPath("readCount").type(JsonFieldType.NUMBER).description("조회수"),
+								fieldWithPath("useYn").type(JsonFieldType.STRING).description("사용여부"),
+								fieldWithPath("registerId").type(JsonFieldType.STRING).description("등록자"),
+								fieldWithPath("registerTimestamp").type(JsonFieldType.STRING).description("등록일시"),
+								fieldWithPath("updateId").type(JsonFieldType.VARIES).description("수정자"),
+								fieldWithPath("updateTimestamp").type(JsonFieldType.VARIES).description("수정일시")
+						)
+				))
 		;
 	}
 
@@ -82,15 +128,54 @@ class ArticleControllerTest {
 				post("/board/{boardId}/article", TEST_BOARD_ID)
 						.content(json)
 						.contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON)
 		)
 				.andExpect(status().isOk())
 				.andDo(print())
-				.andDo(document("postArticle"))
+				.andDo(document("post-article",
+						pathParameters(
+								parameterWithName("boardId").description("게시판ID")
+						),
+						requestFields(
+								fieldWithPath("boardId").type(JsonFieldType.NUMBER).description("게시판ID"),
+								fieldWithPath("noticeYn").type(JsonFieldType.STRING).description("공지여부"),
+								fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
+								fieldWithPath("contents").type(JsonFieldType.STRING).description("내용"),
+								// TODO : 이건좀...;;
+								subsectionWithPath("articleId").description("게시물ID"),
+								subsectionWithPath("parentArticleId").description("parentArticleId"),
+								subsectionWithPath("readCount").description("readCount"),
+								subsectionWithPath("useYn").description("useYn"),
+								subsectionWithPath("registerId").description("registerId"),
+								subsectionWithPath("registerTimestamp").description("registerTimestamp"),
+								subsectionWithPath("updateId").description("updateId"),
+								subsectionWithPath("updateTimestamp").description("updateTimestamp"),
+								subsectionWithPath("searchKey").description("searchKey"),
+								subsectionWithPath("searchWord").description("searchWord"),
+								subsectionWithPath("pageSize").description("pageSize"),
+								subsectionWithPath("pageIndex").description("pageIndex"),
+								subsectionWithPath("countPerPage").description("countPerPage")
+						),
+						responseFields(
+								fieldWithPath("articleId").type(JsonFieldType.NUMBER).description("게시물ID"),
+								fieldWithPath("parentArticleId").type(JsonFieldType.NUMBER).description("부모게시물ID"),
+								fieldWithPath("boardId").type(JsonFieldType.NUMBER).description("게시판ID"),
+								fieldWithPath("noticeYn").type(JsonFieldType.STRING).description("공지여부"),
+								fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
+								fieldWithPath("contents").type(JsonFieldType.STRING).description("내용"),
+								fieldWithPath("readCount").type(JsonFieldType.NUMBER).description("조회수"),
+								fieldWithPath("useYn").type(JsonFieldType.STRING).description("사용여부"),
+								fieldWithPath("registerId").type(JsonFieldType.STRING).description("등록자"),
+								fieldWithPath("registerTimestamp").type(JsonFieldType.STRING).description("등록일시"),
+								fieldWithPath("updateId").type(JsonFieldType.NULL).description("수정자"),
+								fieldWithPath("updateTimestamp").type(JsonFieldType.NULL).description("수정일시")
+						)
+				))
 		;
 	}
 
 	@Test
-	public void purArticle() throws Exception {
+	public void putArticle() throws Exception {
 		ArticleDto dto = new ArticleDto();
 		dto.setTitle("junit test title change");
 		dto.setContents("junit test contents change");
@@ -103,10 +188,50 @@ class ArticleControllerTest {
 						TEST_BOARD_ID, TEST_ARTICLE_ID)
 						.content(json)
 						.contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON)
 		)
 				.andExpect(status().isOk())
 				.andDo(print())
-				.andDo(document("putArticle"))
+				.andDo(document("put-article",
+						pathParameters(
+								parameterWithName("boardId").description("게시판ID"),
+								parameterWithName("articleId").description("게시물ID")
+						),
+						requestFields(
+								fieldWithPath("articleId").type(JsonFieldType.NUMBER).description("게시물ID"),
+								fieldWithPath("boardId").type(JsonFieldType.NUMBER).description("게시판ID"),
+								fieldWithPath("noticeYn").type(JsonFieldType.STRING).description("공지여부"),
+								fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
+								fieldWithPath("contents").type(JsonFieldType.STRING).description("내용"),
+								// TODO : 이건좀...;;
+								subsectionWithPath("parentArticleId").description("parentArticleId"),
+								subsectionWithPath("readCount").description("readCount"),
+								subsectionWithPath("useYn").description("useYn"),
+								subsectionWithPath("registerId").description("registerId"),
+								subsectionWithPath("registerTimestamp").description("registerTimestamp"),
+								subsectionWithPath("updateId").description("updateId"),
+								subsectionWithPath("updateTimestamp").description("updateTimestamp"),
+								subsectionWithPath("searchKey").description("searchKey"),
+								subsectionWithPath("searchWord").description("searchWord"),
+								subsectionWithPath("pageSize").description("pageSize"),
+								subsectionWithPath("pageIndex").description("pageIndex"),
+								subsectionWithPath("countPerPage").description("countPerPage")
+						),
+						responseFields(
+								fieldWithPath("articleId").type(JsonFieldType.NUMBER).description("게시물ID"),
+								fieldWithPath("parentArticleId").type(JsonFieldType.NUMBER).description("부모게시물ID"),
+								fieldWithPath("boardId").type(JsonFieldType.NUMBER).description("게시판ID"),
+								fieldWithPath("noticeYn").type(JsonFieldType.STRING).description("공지여부"),
+								fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
+								fieldWithPath("contents").type(JsonFieldType.STRING).description("내용"),
+								fieldWithPath("readCount").type(JsonFieldType.NUMBER).description("조회수"),
+								fieldWithPath("useYn").type(JsonFieldType.STRING).description("사용여부"),
+								fieldWithPath("registerId").type(JsonFieldType.STRING).description("등록자"),
+								fieldWithPath("registerTimestamp").type(JsonFieldType.STRING).description("등록일시"),
+								fieldWithPath("updateId").type(JsonFieldType.STRING).description("수정자"),
+								fieldWithPath("updateTimestamp").type(JsonFieldType.STRING).description("수정일시")
+						)
+				))
 		;
 	}
 
@@ -116,10 +241,16 @@ class ArticleControllerTest {
 		mockMvc.perform(
 				delete("/board/{boardId}/article/{articleId}",
 						TEST_BOARD_ID, TEST_ARTICLE_ID)
+						.accept(MediaType.APPLICATION_JSON)
 		)
 				.andExpect(status().isOk())
 				.andDo(print())
-				.andDo(document("deleteArticle"))
+				.andDo(document("delete-article",
+						pathParameters(
+								parameterWithName("boardId").description("게시판ID"),
+								parameterWithName("articleId").description("게시물ID")
+						)
+				))
 		;
 	}
 }
