babaad2d7296d1f01f5fdd8b44b1933d87af185e
empering <empering@naver.com>
2020-09-21 17:04:56 +0900

    ArticleModelAssembler 수정
    toCollectionModel 제거

--- src/main/java/kr/re/kari/portal/board/article/ArticleModelAssembler.java
+++ src/main/java/kr/re/kari/portal/board/article/ArticleModelAssembler.java
@@ -1,12 +1,8 @@
 package kr.re.kari.portal.board.article;
 
-import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
-
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -19,28 +15,7 @@ public class ArticleModelAssembler implements RepresentationModelAssembler<Artic
 		return EntityModel.of(article,
 				linkTo(methodOn(ArticleController.class).getArticle(article.getArticleId())).withSelfRel().expand(article.getBoardId()),
 				linkTo(ArticleController.class).withRel("articles").expand(article.getBoardId())
-				// linkTo(methodOn(ArticleController.class).getArticleAll(null, )).withRel("articles").expand(article.getBoardId())
 		);
 	}
 
-	@Override
-	public CollectionModel<EntityModel<Article>> toCollectionModel(Iterable<? extends Article> articles) {
-		return CollectionModel.of(
-				StreamSupport.stream(articles.spliterator(), false)
-						.map(this::toModel)
-						.collect(Collectors.toList()),
-				linkTo(ArticleController.class).withSelfRel()
-				// linkTo(methodOn(ArticleController.class).getArticleAll(null)).withSelfRel()
-		);
-	}
-
-	public CollectionModel<EntityModel<Article>> toCollectionModel(Iterable<? extends Article> articles, Long boardId) {
-		return CollectionModel.of(
-				StreamSupport.stream(articles.spliterator(), false)
-						.map(this::toModel)
-						.collect(Collectors.toList()),
-				linkTo(ArticleController.class).withSelfRel().expand(boardId)
-				// linkTo(methodOn(ArticleController.class).getArticleAll(null)).withSelfRel().expand(boardId)
-		);
-	}
 }
