d747146879688f08ee8aff92c18871a92980c037
empering <empering@naver.com>
2020-09-21 17:28:21 +0900

    test profile 추가
    test 경우 H2Server 등록하지않음

--- src/main/java/kr/re/kari/portal/board/config/H2ServerConfiguration.java
+++ src/main/java/kr/re/kari/portal/board/config/H2ServerConfiguration.java
@@ -5,6 +5,7 @@ import org.h2.tools.Server;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
@@ -13,6 +14,7 @@ import java.sql.SQLException;
 public class H2ServerConfiguration {
 
 	@Bean
+	@Profile("!test")
 	@ConfigurationProperties("spring.datasource.hikari")
 	public DataSource dataSource() throws SQLException {
 		Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "8089").start();
--- src/test/java/kr/re/kari/portal/board/ControllerTest.java
+++ src/test/java/kr/re/kari/portal/board/ControllerTest.java
@@ -7,9 +7,11 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(TestConfig.class)
